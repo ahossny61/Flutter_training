5d1b2a949c68e3706dc85574c36ded4d2a4642c8
ahossny61 <ahossny61@gmail.com>
2022-01-05 16:30:38 +0200

    Design(Stack,Row,Image,Decoration)

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,56 +1,66 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-      appBar:AppBar(
+    return Scaffold(
+        appBar:AppBar(
         leading: Icon(
-          Icons.menu,),
-        title: Text('MY First App With Flutter',),
-        actions: [
-          IconButton(onPressed:onNotification , icon:Icon(Icons.search)),
-          IconButton(onPressed: (){print("hello world");}, icon:  Icon(Icons.notification_important))
-         ],
-        centerTitle: false,
-        elevation: 20.0,
-      ) ,
-       body:Container(
-         color: Colors.purpleAccent,
-         width: double.infinity,
-         child: Column(
-           mainAxisSize: MainAxisSize.max,
-           mainAxisAlignment: MainAxisAlignment.center,
-           crossAxisAlignment: CrossAxisAlignment.end,
-           children: [
-           Container(
-             height: 50,
-             color: Colors.teal,
-             child: Text('Hello world',
-               style: TextStyle(
-                 fontSize: 25,
-
-               ),
-             ),
-
-           ),
-             Container(
-               height: 50,
-               color: Colors.red,
-               child: Text('Hello world 2',
-                 style: TextStyle(
-                   fontSize: 20,
-                 ),
-               ),
-             ),
-      ] ,
+        Icons.menu,),
+    title: Text('MY First App With Flutter',),
+    actions: [
+    IconButton(onPressed:onNotification , icon:Icon(Icons.search)),
+    IconButton(onPressed: (){print("hello world");}, icon: Icon(Icons.notification_important))
+    ],
+    centerTitle: false,
+    elevation: 20.0,
     ),
-       ),
-     );
-  }
-  void onNotification(){
+    body:Container(
+
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      //IMage
+    Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children:[
+            Image(image: NetworkImage('https://www.pikpng.com/pngl/b/301-3014000_view-fullsize-uzumaki-naruto-image-naruto-ultimate-ninja.png'),
+            height: 150,
+            width: 150,
+            fit: BoxFit.cover,
+          ),
+            Container(
+              width: 150,
+              color: Colors.black,
+              padding: const EdgeInsets.all(10),
+              child: Text('Naruto',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+            )
+        ]),
+      ),
+    )
+    ]
+    ,
+    ),
+    ),
+    );
+    }
+
+  void onNotification() {
     print("notification");
   }
 }
